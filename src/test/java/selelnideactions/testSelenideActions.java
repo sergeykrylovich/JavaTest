d69be812c946cd7e6a81aa18fb06a437eb4b9b66
Крылович Сергей Станиславович <siarhei.krylovich@softclub.by>
2023-05-11 09:24:12 +0300

    test dragAndDrop method

--- src/test/java/selelnideactions/testSelenideActions.java
+++ src/test/java/selelnideactions/testSelenideActions.java
@@ -1,10 +1,12 @@
 package selelnideactions;
 
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Configuration.*;
 import static com.codeborne.selenide.Selectors.byTagAndText;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -20,9 +22,26 @@ public class testSelenideActions {
         //sleep(5000);
     }
 
+    @BeforeAll
+    static void beforeAll () {
+        baseUrl = "https://the-internet.herokuapp.com";
+        browser = "chrome";
+        browserSize = "1920x1080";
+    }
     @Test
     void testDragDrop (){
-        
+        // Open drag and drop page
+        open("/drag_and_drop");
+
+        // Drag and drop element with text "A" to element  with text "B" by actions
+        //actions().moveToElement($("#column-a")).clickAndHold().moveByOffset(250, 0).release().perform();
+
+        // use dragAndDrop method to move A to B
+        $("#column-a").dragAndDropTo($("#column-b"));
+
+        // Check that elements swap
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
 
     }
 
